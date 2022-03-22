cb13acd89f91efbae8b410a09ee372a82f48cfff
Albert <sab4856@gmail.com>
2022-03-23 04:55:27 +0800

    feat: 測試 path API

--- 38143856&38143859/app.js
+++ 38143856&38143859/app.js
@@ -1,5 +1,18 @@
-var a = 1;
-var b = 2;
 console.log(__dirname);
 console.log(__filename);
-var c = 3;
+
+var path = require('path');
+// 抓目錄路徑
+console.log(path.dirname('/data/db1/aa.js'));
+
+// 路徑合併
+console.log(path.join(__dirname, '/db1'));
+
+// 抓檔名
+console.log(path.basename('/data/db1/aa.js'));
+
+// 抓副檔名
+console.log(path.extname('/data/db1/aa.js'));
+
+//分析路徑
+console.log(path.parse('/data/db1/aa.js'));
